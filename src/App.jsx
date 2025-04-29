7595c0349a13acc852f25b65b212fd0b366241ad
JoanneOs <jowan.awsman@yahoo.com>
2025-04-29 13:13:19 -0500

    add our components to the App function.

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,36 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
-      <h1>Simple React Application</h1>
-    </header>
+    // <header>
+    //   <h1>Simple React Application</h1>
+    // </header>
+
+    <h1>Simple React Application</h1>
   );
 }
 
 function Content() {
   return (
-    <main>
-      <p>This is my first React Application</p>
-    </main>
+    // <main>
+    //   <p>This is my first React Application</p>
+    // </main>
+    <p>This is my first React Application, wish me luck!</p>
   );
 }
 
 function Footer() {
   return (
-    <footer>
-      <h1>Created by Me, of course.</h1>
-    </footer>
+    // <footer>
+    //   <h1>Created by Me, of course.</h1>
+    // </footer>
+    <h1>Created by Me, of course.</h1>
   );
 }
 
 function App() {
   return (
     <>
-      <Header />
-      <Content />
-      <Footer />
+   
     </>
   );
 }
