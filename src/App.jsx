ea006ea2b6910bd6fddd84da378e7b98f1a4f8e9
JoanneOs <jowan.awsman@yahoo.com>
2025-04-29 13:23:10 -0500

    adding style exat same as example

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// this is the header component
 function Header() {
   return (
     // <header>
@@ -10,13 +11,18 @@ function Header() {
   );
 }
 
-function Content() {
-  return (
-    // <main>
-    //   <p>This is my first React Application</p>
-    // </main>
-    <p>This is my first React Application, wish me luck!</p>
-  );
+// this is the content component that uses props for text and color
+
+function Content(props) {
+  // return (
+  //   // <main>
+  //   //   <p>This is my first React Application</p>
+  //   // </main>
+  //   <p>This is my first React Application, wish me luck!</p>
+  // );
+
+  return (<p style={{ color: props.color }}>{props.text}</p>);
+
 }
 
 function Footer() {
@@ -31,7 +37,11 @@ function Footer() {
 function App() {
   return (
     <>
-   
+   <Header />
+   <Content color="blue" text="this is my first react application!" />
+   <Content color="red" text="Wish me luck..." />
+   <Content color="green" text="I think I've got it!" />
+      <Footer />
     </>
   );
 }
