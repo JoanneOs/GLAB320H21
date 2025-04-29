ac978a3d96998ad81ab4593f3df52a4275a7d2b5
JoanneOs <jowan.awsman@yahoo.com>
2025-04-29 13:09:09 -0500

    adding header, a footer, and a content component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Simple React Application</h1>
+    </header>
+  );
+}
+
+function Content() {
+  return (
+    <main>
+      <p>This is my first React Application</p>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <h1>Created by Me, of course.</h1>
+    </footer>
+  );
+}
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Learn React</a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
   );
 }
 
